bb1408e00af819e093518b3dc84adc68a64f3507
Phạm Kiều Thanh <hhnnaahhtt@gmail.com>
2017-10-21 17:17:26 +0700

    restructure

---
+++ db/db.go
@@ -0,0 +1,94 @@
+package db
+
+import (
+	"github.com/boltdb/bolt"
+	"os"
+	"time"
+)
+
+type LocalDB struct {
+	db *bolt.DB
+	root string
+	database []byte
+}
+
+func NewLocalDB(root, database string) *LocalDB {
+	d := &LocalDB{}
+	d.root = root
+	d.database = []byte(database)
+
+	var err error
+	d.db, err = bolt.Open("git.db", 0600, nil)
+	if err != nil {
+		panic(err)
+	}
+
+	err = d.db.Update(func(tx *bolt.Tx) error {
+		_, err := tx.CreateBucketIfNotExists(d.database)
+		return err
+	})
+	if err != nil {
+		panic(err)
+	}
+	return d
+}
+
+func (m LocalDB) Close() {
+	err := m.db.Close()
+	if err != nil {
+		panic(err)
+	}
+}
+
+func (m LocalDB) IsRepoExists(service, repo string) bool {
+	_, err := os.Stat(m.root + service + "/" +  repo + "/.git")
+	return err == nil
+}
+
+func (m LocalDB) Touch(service, repo string) (err error) {
+	var ut time.Time
+	err = m.db.View(func(tx *bolt.Tx) error {
+		b := tx.Bucket(m.database)
+		utb := b.Get([]byte(service + "/" + repo))
+		ut, _ = time.Parse(time.RFC3339Nano, string(utb))
+		return nil
+	})
+
+	if err != nil {
+		return err
+	}
+
+	if time.Now().Sub(ut).Nanoseconds() < 0 {
+		return nil
+	}
+
+	err = m.db.Update(func(tx *bolt.Tx) error {
+		b := tx.Bucket(m.database)
+		err := b.Put([]byte(service + "/" + repo), []byte(time.Now().Format(time.RFC3339Nano)))
+		if err != nil {
+			return err
+		}
+		return nil
+	})
+
+	return err
+}
+
+func (m LocalDB) LastUpdate(service, repo string) time.Time {
+	var t time.Time
+	err := m.db.View(func(tx *bolt.Tx) error {
+		b := tx.Bucket(m.database)
+		utb := b.Get([]byte(service + "/" + repo))
+		if len(utb) == 0 {
+			t = time.Time{}
+			return nil
+		}
+		ut, _ := time.Parse(time.RFC3339Nano, string(utb))
+		t = ut
+		return nil
+	})
+	if err != nil {
+		return time.Time{}
+	}
+	return t
+}
--- main.go
+++ main.go
@@ -3,138 +3,52 @@ package main
 import (
 	"fmt"
 	"net/http"
-	"html"
-	"io/ioutil"
 	"strings"
-	"os"
-	"github.com/valyala/fasthttp"
 	"time"
-	"github.com/tidwall/gjson"
-	"github.com/boltdb/bolt"
-	"github.com/keegancsmith/shell"
 	"net/http/httputil"
 	"net/url"
+	"github.com/thanhpk/sfgit/db"
+	"github.com/thanhpk/sfgit/git"
+	"github.com/jinzhu/configor"
 )
 
-var db *bolt.DB
-var pushed_bk = []byte("repo_pushed")
-var root = "/srv/git/"
-const (
-	github = "https://github.com/"
-)
-
-var client = &fasthttp.Client{
-	MaxConnsPerHost: 1000,
+type DB interface {
+	IsRepoExists(service, repo string) bool
+	Close()
+	Touch(service, repo string) error
+	LastUpdate(service, repo string) time.Time
 }
 
-var reverseproxy *httputil.ReverseProxy
+type API interface {
+	GetService() string
+	LastUpdate(repo string) time.Time
+	//ShouldUpdate(repo string) bool
+	PullRepo(repo string) error
+	CloneRepo(repo string) error
+}
 
-func sendHTTPRequest(method, url string) string {
-	//now := time.Now().Unix()
-	req := fasthttp.AcquireRequest()
-	req.SetRequestURI(url)
-	req.Header.SetMethod(method)
-	req.Header.SetUserAgent("Gitbot/4.012")
-	res := fasthttp.AcquireResponse()
+var store DB
+var github API
+var bitbucket API
 
-	err := client.DoTimeout(req, res, 4 * time.Second)
-	if err != nil {
-		fmt.Println(err)
+func shouldUpdate(db DB, api API, service, repo string) bool {
+	t := api.LastUpdate(repo)
+	ut := db.LastUpdate(service, repo)
+	if ut.Sub(t).Seconds() < 3 {
+		return true
 	}
-	return string(res.Body())
+	return false
 }
 
-
-func isRepoExist(root, repo string) bool {
-	_, err := os.Stat(root + repo + "/.git")
-	return err == nil
-}
-
-func updateLastUpdated(repo string, t time.Time) (err error) {
-	var ut time.Time
-	err = db.View(func(tx *bolt.Tx) error {
-		b := tx.Bucket(pushed_bk)
-		utb := b.Get([]byte(repo))
-		ut, _ = time.Parse(time.RFC3339Nano, string(utb))
-		return nil
-	})
-
-	if err != nil {
-		return err
-	}
-
-	if t.Sub(ut).Nanoseconds() < 0 {
-		return nil
-	}
-
-	err = db.Update(func(tx *bolt.Tx) error {
-		b := tx.Bucket(pushed_bk)
-		err := b.Put([]byte(repo), []byte(t.Format(time.RFC3339Nano)))
-		if err != nil {
-			return err
-		}
-		return nil
-	})
-
-	return err
-}
-
-func updateIfOutdated(root, repo string) {
-	if !shouldUpdate(root, repo) {
+func updateIfOutdated(db DB, api API, service, repo string) {
+	if !shouldUpdate(db, api, service, repo) {
 		return
 	}
-
-	err := fetchRepo(root, repo)
+	err := api.PullRepo(repo)
 	if err != nil {
 		fmt.Println(err)
 	}
-	updateLastUpdated(repo, time.Now())
-}
-
-func shouldUpdate(root, repo string) bool {
-	data := sendHTTPRequest("GET", "https://api.github.com/repos/" + repo)
-	pushed := gjson.Get(data, "pushed_at")
-	t, err := time.Parse(time.RFC3339Nano, pushed.Str)
-	if err != nil {
-		fmt.Println(err)
-		return true
-	}
-	var sh bool
-	err = db.View(func(tx *bolt.Tx) error {
-		b := tx.Bucket(pushed_bk)
-		utb := b.Get([]byte(repo))
-		if len(utb) == 0 {
-			sh = true
-			return nil
-		}
-		ut, _ := time.Parse(time.RFC3339Nano, string(utb))
-		if ut.Sub(t).Seconds() < 3 {
-			sh = true
-			return nil
-		}
-		sh = false
-		return nil
-	})
-
-	if err != nil {
-		return true
-	}
-
-	return sh
-}
-
-func cloneRepo(root, repo string) {
-	cmd := shell.Commandf("git clone %s %s", github + repo, root + repo)
-	err := cmd.Run()
-	if err != nil {
-		fmt.Println(err)
-	}
-}
-
-func fetchRepo(root, repo string) error {
-	println("fetching")
-	cmd := shell.Commandf("cd %s && for remote in `git branch -r `; do git branch --track $remote; done || git remote update && git pull --all", root + repo)
-	return cmd.Run()
+	db.Touch(service, repo)
 }
 
 func extractRepo(url string) string {
@@ -143,118 +57,53 @@ func extractRepo(url string) string {
 }
 
 func handler(w http.ResponseWriter, r *http.Request) {
+	var api API
+	var reurl *url.URL
+	if strings.Contains(r.Host, "github") {
+		api = github
+	} else if strings.Contains(r.Host, "bitbucket") {
+		api = bitbucket
+	} else {
+		http.Error(w, "unsupported service " + r.Host, 400)
+		return
+	}
+	reurl, _ = url.Parse("http://127.0.0.1:" + Config.GitPort + "/" + api.GetService() + "/")
+
+
 	if r.Method == "GET" {
 		repo := extractRepo(r.URL.Path)
-		if isRepoExist(root, repo) {
-			updateIfOutdated(root, repo)
+		if store.IsRepoExists(api.GetService(), repo) {
+			updateIfOutdated(store, api, api.GetService(), repo)
 		} else {
-			cloneRepo(root, repo)
+			err := api.CloneRepo(repo)
+			if err != nil {
+				fmt.Println(err)
+			}
 		}
 	}
+	reverseproxy := httputil.NewSingleHostReverseProxy(reurl)
 	reverseproxy.ServeHTTP(w, r)
 }
 
-func listHandler(w http.ResponseWriter, r *http.Request) {
-	var root = "/git"
-	//flusher := w.(http.Flusher)
-	repochan := getHead(root, listAllGitRepo(root))
-	for r := range repochan {
-		fmt.Fprint(w, "<code>" + html.EscapeString(r.commit)  + "</code>&nbsp;" + html.EscapeString(r.name) + "<br/>")
+var Config = struct {
+	Database string `default:"pushed_bk"`
+	Root string `default:"/srv/git/"`
+	Bitbucket struct {
+		Email, Password string
 	}
-}
-
-type Repo struct {
-	name, commit string
-}
-
-func getHead(root string, in <-chan string) <-chan Repo {
-	repochan := make(chan Repo)
-	go func() {
-		for rname := range in {
-			repochan <- Repo{
-				name: rname,
-				commit: getLatestCommit(root + "/" + rname),
-			}
-		}
-		close(repochan)
-	}()
-	return repochan
-}
-
-func listAllGitRepo(root string) <-chan string {
-	repochan := make(chan string)
-	go func() {
-		defer func() {
-			r := recover()
-			if r != nil {
-				fmt.Println(r)
-			}
-			close(repochan)
-		}()
-		files, err := ioutil.ReadDir(root)
-		if err != nil {
-			fmt.Println(err)
-			return
-		}
-		for _, f := range files {
-			if !f.IsDir() {
-				continue
-			}
-
-			repfiles, err := ioutil.ReadDir(root + "/" + f.Name())
-			if err != nil {
-				fmt.Println(err)
-				continue
-			}
-			for _, r := range repfiles {
-				if !f.IsDir() {
-					continue
-				}
-				repochan <- f.Name() + "/" + r.Name()
-			}
-		}
-	}()
-	return repochan
-}
-
-func getLatestCommit(repo string) string {
-	head, err := ioutil.ReadFile(repo + ".git/HEAD")
-	if err != nil {
-		return ""
-	}
-	commit, err := ioutil.ReadFile(repo + ".git/" + string(head))
-	if err != nil {
-		return ""
-	}
-	return string(commit)
-}
-
-func setupDB() *bolt.DB {
-	db, err := bolt.Open("git.db", 0600, nil)
-	if err != nil {
-		panic(err)
-	}
-
-	err = db.Update(func(tx *bolt.Tx) error {
-		_, err := tx.CreateBucketIfNotExists(pushed_bk)
-		return err
-	})
-	if err != nil {
-		panic(err)
-	}
-	return db
-}
+	ListenPort string `default:"10292"` // if this change, git.conf should change too
+	GitPort string `default:"12085"` // if this change, git.conf should change too
+}{}
 
 func main() {
-	reurl, err := url.Parse("http://127.0.0.1:12085/")
-	if err != nil {
-		panic(err)
-	}
-	reverseproxy = httputil.NewSingleHostReverseProxy(reurl)
-	db = setupDB()
-	defer db.Close()
+	configor.Load(&Config, "./config.yaml")
+	store = db.NewLocalDB(Config.Root, Config.Database)
+	defer store.Close()
+
+	github = git.NewGithubAPI(Config.Root + "github.com/")
+	bitbucket = git.NewBitbucketAPI(Config.Root + "bitbucket.org/", Config.Bitbucket.Email, Config.Bitbucket.Password)
+
 	http.HandleFunc("/", handler)
-	//http.HandleFunc("/list", listHandler)
-	println("server is running at port 10292")
-	http.ListenAndServe(":10292", nil)
+	fmt.Println("server is running at port " + Config.ListenPort)
+	http.ListenAndServe(":" + Config.ListenPort, nil)
 }
