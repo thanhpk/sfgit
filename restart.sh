265fdfc9792f71453910255acd32555dd31eb267
Phạm Kiều Thanh <hhnnaahhtt@gmail.com>
2017-11-03 15:19:10 +0700

    enable cached build

--- restart.sh
+++ restart.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-CGO_ENABLED=0 go build . &&
+CGO_ENABLED=0 go build -i . &&
 		docker build -qt thanhpk/sfgit . &&
 		docker rm -f sfgittest &&
 		docker run --name sfgittest -v /srv/sfgittest/git:/srv/git -it thanhpk/sfgit
